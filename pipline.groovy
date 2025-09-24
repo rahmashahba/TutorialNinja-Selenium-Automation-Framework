c1e7fc42f21d59eb70d81fd2f259e085be448959
Rahma shahba <dev@test.com>
2025-09-24 22:24:00 +0300

    update pipline.groovy file

--- pipline.groovy
+++ pipline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Get Code') {
             steps {
-                git changelog: false, poll: false, url: 'https://github.com/SalmaMamdouh12/WebScrapping.git'
+                git changelog: false, poll: false, url: ' https://github.com/rahmashahba/tutorialsninja-selenium-automation-framework-.git'
             }
         }
 
